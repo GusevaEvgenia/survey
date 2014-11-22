ef0df495e3a927af2ce38cf709425373cc2f4da6
GusevaEvgenia <ainege@gmail.com>
2014-11-22 13:25:36 +0300

    temp: remove dao objects

--- src/main/java/com/survey/mvc/DAO/UserDAO.java
+++
@@ -1,9 +0,0 @@
-package com.survey.mvc.dao;
-
-import com.survey.mvc.entity.UsersEntity;
-
-public interface UserDAO {
-    public void addUser(UsersEntity user);
-    public void updateUser(UsersEntity user);
-    public UsersEntity getUser(int id);
-}
